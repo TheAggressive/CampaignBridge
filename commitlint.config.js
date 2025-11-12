1e082957a5ed504ef88990669fbd2723eac3cc8e
Shawn Mosher <10968229+TheAggressive@users.noreply.github.com>
2025-11-11 19:35:16 -0700

    chore: increase max line length for commit messages
    - Updated 'header-max-length' and 'body-max-line-length' in commitlint configuration from 100 to 200.
    - Allows for more detailed commit messages while maintaining formatting standards.

--- commitlint.config.js
+++ commitlint.config.js
@@ -21,7 +21,7 @@ export default {
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 100],
-    'body-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 200],
+    'body-max-line-length': [2, 'always', 200],
   },
 };
